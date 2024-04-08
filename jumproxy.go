161786d7e185fa45471e426a5b72470078326af8
nmahamud <nazif.mahamud@stonybrook.edu>
2024-04-08 01:30:28 -0400

    got encryption funcs

--- jumproxy.go
+++ jumproxy.go
@@ -2,51 +2,123 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
 	"fmt"
+	"io"
 	"net"
 	"os"
-        "io"
+	// "encoding/hex"
+	"golang.org/x/crypto/pbkdf2"
+	"log"
 )
+
 const (
-        SERVER_HOST = "localhost"
-        SERVER_PORT = "9988"
-        SERVER_TYPE = "tcp"
+	SERVER_HOST   = "localhost"
+	SERVER_PORT   = "9988"
+	SERVER_TYPE   = "tcp"
+	SALT          = "Honkai Star Rail"
+	OUTGOING_PORT = "9989"
+	TEST_PHRASE   = "Kafka"
 )
+
+var (
+        
+)
+
 func main() {
-        fmt.Println("Server Running...")
-        server, err := net.Listen(SERVER_TYPE, SERVER_HOST+":"+SERVER_PORT)
-        if err != nil {
-                fmt.Println("Error listening:", err.Error())
-                os.Exit(1)
-        }
-        defer server.Close()
-        fmt.Println("Listening on " + SERVER_HOST + ":" + SERVER_PORT)
-        fmt.Println("Waiting for client...")
-        for {
-                connection, err := server.Accept()
-                if err != nil {
-                        fmt.Println("Error accepting: ", err.Error())
-                        os.Exit(1)
-                }
-                fmt.Println("client connected")
-                go processClient(connection)
-        }
+	args := os.Args
+	for index, element := range args {
+		fmt.Println(index, ": ", element)
+	}
+
+        // Start of server test, will change later
+	fmt.Println("Server Running...")
+	server, err := net.Listen(SERVER_TYPE, SERVER_HOST+":"+SERVER_PORT)
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	defer server.Close()
+	fmt.Println("Listening on " + SERVER_HOST + ":" + SERVER_PORT)
+	fmt.Println("Waiting for client...")
+	for {
+		connection, err := server.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		fmt.Println("client connected")
+		go processClient(connection)
+	}
 }
 func processClient(connection net.Conn) {
-        for {
-        buffer := make([]byte, 1024)
-        mLen, err := connection.Read(buffer)
-        if err != nil {
-                if err == io.EOF {
-                        break
-                }
-                fmt.Println("Error reading:", err.Error())
-        }
-        if (string(buffer[:mLen]) == "exit\n") {
-                fmt.Println("Exit!!!!")
-        }
-        fmt.Print("Received: ", string(buffer[:mLen]))
-        _, err = connection.Write([]byte("Thanks! Got your message:" + string(buffer[:mLen])))
+	for {
+		buffer := make([]byte, 1024)
+		mLen, err := connection.Read(buffer)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("Error reading:", err.Error())
+		}
+		if string(buffer[:mLen]) == "exit\n" {
+			fmt.Println("Exit!!!!")
+		}
+		fmt.Print("Before Received: ", string(buffer[:mLen]))
+		encryptedText := encryptIt(buffer[:mLen], TEST_PHRASE)
+		fmt.Println("Encrypted Received: ", string(encryptedText))
+		decryptedText := decryptIt(encryptedText, TEST_PHRASE)
+		fmt.Println("Decrypted Received: ", string(decryptedText))
+		_, err = connection.Write([]byte("Thanks! Got your message:" + string(buffer[:mLen])))
+	}
+	connection.Close()
 }
-        connection.Close()
+
+func pbkdf2Key(input string) []byte {
+	byteInput := []byte(input)
+	key := pbkdf2.Key(byteInput, []byte(SALT), 4096, 32, sha256.New)
+	return key // by referring to it as a string
+}
+
+func encryptIt(value []byte, keyPhrase string) []byte {
+	aesBlock, err := aes.NewCipher([]byte(pbkdf2Key(keyPhrase)))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	gcmInstance, err := cipher.NewGCM(aesBlock)
+	if err != nil {
+		fmt.Println(err)
+	}
+	nonce := make([]byte, gcmInstance.NonceSize())
+	_, _ = io.ReadFull(rand.Reader, nonce)
+
+	cipheredText := gcmInstance.Seal(nonce, nonce, value, nil)
+
+	return cipheredText
+
+}
+
+func decryptIt(ciphered []byte, keyPhrase string) []byte {
+	aesBlock, err := aes.NewCipher([]byte(pbkdf2Key(keyPhrase)))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	gcmInstance, err := cipher.NewGCM(aesBlock)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	nonceSize := gcmInstance.NonceSize()
+	nonce, cipheredText := ciphered[:nonceSize], ciphered[nonceSize:]
+
+	originalText, err := gcmInstance.Open(nil, nonce, cipheredText, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return originalText
+
 }
